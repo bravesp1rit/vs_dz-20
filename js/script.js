b01233e9f2c8d978576911cdd04542ef354b1ee5
brave <progpavlovmark@gmail.com>
2023-02-20 01:11:39 +0300

    dz-20 1.0

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const litersOfBorsch = 48;
+
+const calculation = function (liters){ 
+    const PotatoNeededFor1Liter = 4;
+    const averagePotatoWeight = 75;
+    const priceOfPotato = 13;
+    let calcPotatoNeeded = liters * PotatoNeededFor1Liter;
+    let calcKgOfPotatoNeeded = calcPotatoNeeded / (1000 / averagePotatoWeight);
+    let calcFinalPrice = calcKgOfPotatoNeeded * priceOfPotato;
+    return calcFinalPrice;
+}
+
+console.log(calculation(litersOfBorsch));
